7714b1584ef439080353a8eb260d54a2ede1ada4
dadadon <davianberoni@yahoo.com>
2023-08-05 11:36:48 -0500

    Update files

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Greeting.create(message: 'Hello World!')
-Greeting.create(message: 'Hi there!')
-Greeting.create(message: 'Welcome!')
-Greeting.create(message: 'Greetings!')
-Greeting.create(message: 'Hola!')
+Message.create(content: 'Hello World!')
+Message.create(content: 'Hi there!')
+Message.create(content: 'Welcome!')
+Message.create(content: 'Greetings!')
+Message.create(content: 'Hola!')
